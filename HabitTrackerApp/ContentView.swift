a57cbaddd5b22eec4bfd1c2206303b81e0f8a9c5
alvinjhson <alvinbest26@gmail.com>
2024-04-22 10:15:46 +0200

    Initial Commit

---
+++ HabitTrackerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HabitTrackerApp
+//
+//  Created by Alvin Johansson on 2024-04-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HabitTrackerApp/HabitTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerAppApp.swift
+//  HabitTrackerApp
+//
+//  Created by Alvin Johansson on 2024-04-22.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
